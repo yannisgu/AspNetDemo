f0a04bc5cdbf32a9e431adf77dcbd2fdb7ed1d23
Yannis Guedel <me@yannisguedel.ch>
2015-07-03 23:14:57 +0200

    initital commit

---
+++ AspNetDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNetDemo.MvcApplication" Language="C#" %>
---
+++ AspNetDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetDemo.Startup))]
+namespace AspNetDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
